ebeca9d77a81acd7d79fd2746895572160526c4d
lndgalante <lndgalante@gmail.com>
2019-08-25 20:25:27 -0300

    Improve OrganizationChart structure

--- src/components/OrganizationChart/index.js
+++ src/components/OrganizationChart/index.js
@@ -1,36 +1,3 @@
-import React from 'react';
-import RorgChart from '@lndgalante/r-orgchart';
-import PropTypes from 'prop-types';
-
-const OrganizationChart = ({ isNodeEditionDisabled, isRootEditionDisabled, data, onAddNewChild, onNodeClick }) => {
-  return (
-    <RorgChart
-      data={data}
-      disableRootEdit={isRootEditionDisabled}
-      disableEditNodes={isNodeEditionDisabled}
-      onNodeClick={onNodeClick}
-      addNewChild={onAddNewChild}
-    />
-  );
-};
-
-OrganizationChart.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      title: PropTypes.string,
-      ParentId: PropTypes.number,
-    })
-  ).isRequired,
-  onNodeClick: PropTypes.func.isRequired,
-  onAddNewChild: PropTypes.func.isRequired,
-  isRootEditionDisabled: PropTypes.bool,
-  isNodeEditionDisabled: PropTypes.bool,
-};
-
-OrganizationChart.defaultProps = {
-  isRootEditionDisabled: true,
-  isNodeEditionDisabled: true,
-};
+import OrganizationChart from './OrganizationChart';
 
 export default OrganizationChart;
