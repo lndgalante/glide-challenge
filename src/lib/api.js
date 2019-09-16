62a64175571d972907cdc276dc7af4c3fe744952
lndgalante <lndgalante@gmail.com>
2019-09-15 21:41:39 -0300

    Fix env variable

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? ''
+    ? 'https://2jdg5klzl0.execute-api.us-west-1.amazonaws.com/default/EmployeesChart-Api'
     : 'https://2jdg5klzl0.execute-api.us-west-1.amazonaws.com/default/EmployeesChart-Api';
 
 const api = {
